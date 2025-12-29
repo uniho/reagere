3320303d2e8dbf16e25a6e69b13d5d92e5eeda4a
uniho <uniho@users.noreply.github.com>
2025-12-29 15:52:45 +0900

    Prepare core build scripts and move core source to src/core

---
+++ scripts/build-core.mjs
@@ -0,0 +1,17 @@
+import esbuild from 'esbuild'
+
+//
+await esbuild.build({
+  entryPoints: [
+    "../src/core/index.js",
+  ],
+  outdir: 'lib',
+  bundle: true,
+  format: 'esm',
+  platform: 'browser',
+  target: 'es2018',
+  sourcemap: false,
+  minify: true,
+})
+
+console.log('✅ build complete')
